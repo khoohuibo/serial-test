e52f1dbf3441806fd424a87a65e4a0958488576c
khoohuibo <33455309+khoohuibo@users.noreply.github.com>
2018-10-30 11:44:31 +0000

    Added main.c and MakeFile
    Initial format for minor serial testing of sending and receiving data

---
+++ main.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <libpq-fe.h>
+
+#include "arduino-serial/arduino-serial-lib.c"
+
+char AzimuthData[] = "3685:\0";
+char ElevationData[] = "1834:\0" ;
+char sensorData[20];
+int main(int argc, char const *argv[]) {
+
+  int fd = serialport_init(filename, 9600);
+
+  while(1) {
+     int bytesSent_1 = serialport_write(fd, AzimuthData);
+     if(bytesSent_1 == -1) {
+        printf("Error: Azimuth Data failed to send!\n" );
+      } else {
+        printf("Azimuth : %s sent\n", AzimuthData);
+      }
+      int bytesSent_2 = serialport_write(fd, ElevationData);
+      if(bytesSent_2 == -1) {
+         printf("Error: Elevation Data failed to send!\n" );
+      } else {
+        printf("Elevation : %s sent\n", ElevationData);
+      }
+     int bytesReceived = serialport_read_until(fd, sensorData, ':');
+     if (bytesReceived == -1){
+       printf("Error: Serial Read function failed!\n");
+
+     }
+
+     printf("From Arduino Serial Debug : %s, %s\n", sensorData[0], sensorData[1])
+
+     }
+
+
+
+  return 0;
+}
